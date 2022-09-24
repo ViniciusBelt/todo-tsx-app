4ba0afb79560019e1f7d409ec162bc52a7bfd22e
Vinicius Beltran <super.vi@hotmail.com>
2022-09-23 21:07:18 -0300

    Função para tarefa realizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { useState } from 'react'
 import * as C from './App.styles'
+import { Item } from './types/Item'
+import { ListItem } from './components/ListItem'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão', done: false },
+    { id: 2, name: 'Comprar bolo', done: true },
+  ])
+
   return (
     <C.Container>
       <C.Area>
-        <C.Header>
-          Lista de Tarefas
-        </C.Header>
+        <C.Header>Lista de Tarefas</C.Header>
+        {/* Área de nova tarefa */}
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
       </C.Area>
     </C.Container>
-  );
+  )
 }
 
 export default App;
